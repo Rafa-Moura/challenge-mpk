a2ee2210459596ca07025b9317211116b3a0fca8
Rafael Moura <rafaelfelipe.peixoto@gmail.com>
2022-05-03 10:11:52 -0300

    Include bean javax validation

--- src/main/java/com/rafaelmoura/challenge/models/Cliente.java
+++ src/main/java/com/rafaelmoura/challenge/models/Cliente.java
@@ -1,7 +1,75 @@
 package com.rafaelmoura.challenge.models;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+@Entity
 public class Cliente {
 
-	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String cpnj;
+	@Min(value = 18, message = "O cliente não pode ser menor de 18 anos")
+	private Integer idade;
+	private Date dataNascimento;
+
+	public Cliente() {
+
+	}
+
+	public Cliente(String nome, String cpnj, Integer idade, Date dataNascimento) {
+		super();
+		this.nome = nome;
+		this.cpnj = cpnj;
+		this.idade = idade;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpnj() {
+		return cpnj;
+	}
+
+	public void setCpnj(String cpnj) {
+		this.cpnj = cpnj;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
 }
