a895d1841e640d3b32258f732686d0832fb2f5c2
Rafael Moura <rafaelfelipe.peixoto@gmail.com>
2022-05-03 09:46:46 -0300

    Project created

---
+++ src/main/java/com/rafaelmoura/challenge/ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmoura.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeApplication.class, args);
+	}
+
+}
